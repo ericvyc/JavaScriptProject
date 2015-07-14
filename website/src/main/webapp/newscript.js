6c98e795483e3888d8d9616cefdf44062236d047
ericvyc <eric_vilar2007@hotmail.com>
2015-07-14 11:05:30 -0300

    primeiro commit

---
+++ website/src/main/webapp/newscript.js
@@ -0,0 +1,21 @@
+var a = 10;
+
+document.writeln("Pós-incremento de 10:");
+document.writeln(a++);
+document.writeln("</br>");
+document.writeln("Pré-incremento de 10:");
+document.writeln(++a);
+document.writeln("</br>");
+document.writeln("Pós-decremento de 10:");
+document.writeln(a--);
+document.writeln("</br>");
+document.writeln("Pré-decremento de 10:");
+document.writeln(--a);
+document.writeln("</br>");
+
+var notaRand = Math.random();
+
+document.writeln(notaRand);
+document.writeln("</br>");
+document.writeln(notaRand >= 0.5 ? "aprovado" : "reprovado");
+console.log("Celoko cachueira");
---
+++ website/src/main/webapp/script.js
@@ -0,0 +1,13 @@
+    var valor1 = 10;
+    var valor2 = 3.14;
+    var boolean = true;
+    var und;
+    document.writeln(valor1 + " é um valor inteiro, já " + valor2 + " é um ponto flutuante!");
+    document.writeln("Teste de arquivo externo");
+    document.writeln(boolean);
+    document.writeln(und);
+    var i = 10;
+   
+    var j = ++i + i --;
+   
+    alert (j);
